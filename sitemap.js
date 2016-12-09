589544ac4fc62eb6456bf1269d78507762b35d88
DrewGregor-y <Mtgoplayer14@gmail.com>
2016-12-09 10:35:02 -0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,27 +1,27 @@
 function buildMap(id){
- var sitemap = getElementById(id);
- var list = document.createElement("ul");
- sitemap.appendChild(list);
- var index = new Array("home","index.html",
- "Bio", "Bio.html",
- "Formula","Formula.html",
- "Multiplication Table","multiplicaiontable.html",
- "Breakout", "breakout.html",
- "Multiplication Slider", "MultiplicationTablePlus",
- "Physics Simulator", "CircularMotion.html",
- "Chalkboard","Chalkboard.html",
- "Pop-up","Popup.html",
- "Tic Tac Toe","TicTacToe.html",
- "Html Test","htmltest.html",
- "Index","index.html",
- "To Do List","todolist.html",
- "Sitemap","sitemap.html");
- for(var i=0; i<26; i=i+2){
- var li= document.createElement("li");
- ul.appendChild(li);
- var link = document.createElement("a");
- a.textContent= index[i];
- a.href= index[i+1];
- li.appendChild(link);
- }
+    var map = document.getElementById(id);
+    var list = document.createElement("ul");
+    map.appendChild(list);
+    var index = new Array(
+    "Index","index.html",
+    "Bio", "Bio.html",
+    "Formula","Formula.html",
+    "Multiplication Table","multiplicaiontable.html",
+    "Breakout", "breakout.html",
+    "Multiplication Slider", "MultiplicationTablePlus.html",
+    "Physics Simulator", "CircularMotion.html",
+    "Chalkboard","Chalkboard.html",
+    "Pop-up","Popup.html",
+    "Tic Tac Toe","TicTacToe.html",
+    "Html Test","htmltest.html",
+    "To Do List","todolist.html",
+    "Sitemap","sitemap.html");
+    for(var i=0; i<25; i=i+2){     
+        var li= document.createElement("li");
+        list.appendChild(li);
+        var link = document.createElement("a");
+        link.textContent= index[i];
+        link.href= index[i+1];
+        li.appendChild(link);    
+    }   
 }
